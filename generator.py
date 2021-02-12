dcde312b85556404a563c44b9cac9c58fb72a67d
Grant Patterson <grant.patterson@gmail.com>
2021-02-11 20:52:46 -0800

    Generator working

--- generator.py
+++ generator.py
@@ -4,12 +4,13 @@ from PIL import Image
 import random
 
 HASHTAG = 'lifecubeproject'
-LIMIT = 25
-MAX_RES = 8192
+LIMIT = None
+MAX_RES = 4096
 
 imagedir = f'images-{HASHTAG}'
-#TODO: --fast-update? For each target, stop when encountering the first already-downloaded picture. This flag is recommended when you use Instaloader to update your personal Instagram archive.
-#os.system(f'instaloader "#{HASHTAG}" --no-videos --no-metadata-json --no-captions --no-profile-pic --dirname-pattern="{imagedir}"')
+outdir = f'textures-{HASHTAG}'
+os.makedirs(outdir, exist_ok=True)
+os.system(f'instaloader "#{HASHTAG}" --fast-update --no-videos --no-metadata-json --no-captions --no-profile-pic --dirname-pattern="{imagedir}"')
 
 files = [f for f in os.listdir(imagedir) if f.endswith('.jpg')]
 random.shuffle(files)
@@ -29,13 +30,33 @@ if nimg > len(files):
     files += files[:nimg-len(files)]
     random.shuffle(files)
 
-for dim in dims:
+for (i,dim) in enumerate(dims):
     dfiles = files[:dim**2]
     files = files[dim**2:]
-    maxdim = 0
-    for i in range(len(dfiles)):
-        dfiles[i] = Image.open(os.path.join(imagedir, dfiles[i]))
+    size = 0
+    for f in dfiles:
+        img = Image.open(os.path.join(imagedir, f))
         # Only use the min of width/height because we crop to make squares
-        maxdim = max(min(dfiles[i].width, dfiles[i].height), maxdim)
-    size = min(MAX_RES, maxdim * dim)
-    print("dim", dim, size)
+        size = max(min(img.width, img.height), size)
+        img.close()
+    size = min(math.floor(MAX_RES / dim), size)
+    print("dim", i, dim, size)
+
+    face = Image.new('RGB', (size*dim, size*dim))
+    for (j,f) in enumerate(dfiles):
+        img = Image.open(os.path.join(imagedir, f))
+        if img.width != size or img.height != size:
+            src_size = min(img.width, img.height)
+            x = 0
+            y = 0
+            if img.width > src_size:
+                x = (img.width-src_size) // 2
+            if img.height > src_size:
+                y = (img.height-src_size) // 2
+            img = img.resize((size, size), Image.LANCZOS,
+                             (x, y, x+src_size, y+src_size))
+        row = j // dim
+        col = j - row*dim
+        face.paste(img, (row*size, col*size))
+        img.close()
+    face.save(os.path.join(outdir, f'{HASHTAG}-{i}.jpg'))
